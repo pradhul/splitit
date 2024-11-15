145f95c3706fdce416a4ddeb8006ad07dc077f3e
Pradhul <pradhuldev.1990@gmail.com>
2024-11-16 01:41:23 +0530

    react query and devtools

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,10 +1,20 @@
 import { Titles } from "@/constants/Strings";
 import { Stack } from "expo-router";
+import { DevToolsBubble } from "react-native-react-query-devtools";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+
+const queryClient = new QueryClient();
 
 export default function RootLayout() {
   return (
-    <Stack screenOptions={{headerShown: true, title: Titles.recordPaymentPage}} >
-      <Stack.Screen name="index" />
-    </Stack>
+    <QueryClientProvider client={queryClient}>
+      <Stack
+        screenOptions={{ headerShown: true, title: Titles.recordPaymentPage }}
+      >
+        <Stack.Screen name="index" />
+        <DevToolsBubble />
+      </Stack>
+      <DevToolsBubble />
+    </QueryClientProvider>
   );
 }
