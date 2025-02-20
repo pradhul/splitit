a6fdaa63f3b707356ca3c78e4cd71facfd289d2f
Pradhul <pradhuldev.1990@gmail.com>
2025-02-20 10:18:32 +0530

    tests for button

--- components/__tests__/PrimaryButton-test.tsx
+++ components/__tests__/PrimaryButton-test.tsx
@@ -8,24 +8,97 @@
 import { render, screen } from "@testing-library/react-native";
 import PrimaryButton from "@/components/PrimaryButton";
 import { ReactTestInstance } from "react-test-renderer";
+import { ActivityIndicator, Text } from "react-native";
+import MockedMaterialIcons from "@expo/vector-icons/MaterialIcons";
+
+jest.mock("@expo/vector-icons/MaterialIcons", () => {
+  const { View } = require("react-native");
+  return jest.fn((props) => <View {...props} />);
+});
 
 describe("<PrimaryButton />", () => {
   let element: ReactTestInstance;
 
-  beforeEach(() => {
-    render(<PrimaryButton />);
-  });
-
-  test("text displays in correct style", () => {
+  test("text should display in correct style", () => {
+    render(<PrimaryButton title="Record Payment" onPress={jest.fn()} />);
     const element = screen.getByText("Record Payment");
 
     expect(element).toBeTruthy();
-    expect(element).toHaveStyle({ color: "#424242" });
+    expect(element).toHaveStyle({ color: "#DCE0D9" });
   });
 
-  test("Button has the correct background color", () => {
+  test("Button should have the correct background color", () => {
+    render(<PrimaryButton title="Record Payment" onPress={jest.fn()} />);
     const element = screen.getByTestId("primary-button");
 
-    expect(element).toHaveStyle({ backgroundColor: "#00796B" });
+    expect(element).toHaveStyle({ backgroundColor: "#5B1865" });
+  });
+
+  test("Disabled Button should have backGroundColor as #808080", () => {
+    render(<PrimaryButton title="Record Payment" onPress={jest.fn()} disabled />);
+    const element = screen.getByTestId("primary-button");
+
+    expect(element).toHaveStyle({ backgroundColor: "#808080" });
+  });
+
+  test("A button of status 'pending' should have ActivityIndicator as buttonContent", () => {
+    render(<PrimaryButton title="Record Payment" onPress={jest.fn()} status="pending" />);
+    const element = screen.getByTestId("primary-button");
+
+    const activityIndicator = element.findByType(ActivityIndicator);
+    expect(activityIndicator).toBeTruthy();
+    expect(activityIndicator).toMatchObject({
+      type: ActivityIndicator,
+      props: {
+        size: "small",
+        color: "#fff",
+      },
+    });
+  });
+
+  test("A button of status 'success' should have 'done' of MaterialIcon as buttonContent", () => {
+    render(<PrimaryButton title="Record Payment" onPress={jest.fn()} status="success" />);
+    const element = screen.getByTestId("primary-button");
+
+    const buttonContent = element.findByType(MockedMaterialIcons);
+    expect(buttonContent).toBeTruthy();
+    expect(buttonContent).toMatchObject({
+      type: MockedMaterialIcons,
+      props: {
+        style: { alignSelf: "center" },
+        name: "done",
+        size: 24,
+      },
+    });
+  });
+
+  test("A button of status 'error' should have 'error' of MaterialIcons as buttonContent", () => {
+    render(<PrimaryButton title="Record Payment" onPress={jest.fn()} status="error" />);
+    const element = screen.getByTestId("primary-button");
+
+    const buttonContent = element.findByType(MockedMaterialIcons);
+    expect(buttonContent).toBeTruthy();
+    expect(buttonContent).toMatchObject({
+      type: MockedMaterialIcons,
+      props: {
+        style: { alignSelf: "center" },
+        name: "error",
+        size: 24,
+      },
+    });
+  });
+
+  test("The default PrimaryButton component should be enabled by default with buttonContent as Text", () => {
+    render(<PrimaryButton title="Record Payment" onPress={jest.fn()} />);
+    const element = screen.getByTestId("primary-button");
+    const buttonContent = element.findByType(Text);
+
+    expect(element).toBeTruthy();
+    expect(element).toBeEnabled();
+    expect(element.props.accessibilityState).toMatchObject({
+      disabled: false,
+    });
+
+    expect(buttonContent).toBeTruthy();
   });
 });
